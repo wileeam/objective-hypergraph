29f679e1b045ccec53f48e8c62398922067ba2b9
Guillermo <wileeam@users.noreply.github.com>
2011-04-20 00:35:07 +0200

    Initialisation: Initial import of XCode project

---
+++ Objective-HyperGraphTests/Objective_HyperGraphTests.h
@@ -0,0 +1,17 @@
+//
+//  Objective_HyperGraphTests.h
+//  Objective-HyperGraphTests
+//
+//  Created by Guillermo on 20/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface Objective_HyperGraphTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ Objective-HyperGraphTests/Objective_HyperGraphTests.m
@@ -0,0 +1,33 @@
+//
+//  Objective_HyperGraphTests.m
+//  Objective-HyperGraphTests
+//
+//  Created by Guillermo on 20/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Objective_HyperGraphTests.h"
+
+
+@implementation Objective_HyperGraphTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Objective-HyperGraphTests");
+}
+
+@end
